430856222f77ffa83a8961b81423f5f7af794616
Roberto Fuentes <roberfuentesg@gmail.com>
2024-01-08 03:18:24 -0300

    Init project

---
+++ src/main/java/cl/springmachine/file/storage/spring/boot/FileStorageSpringBootApplication.java
@@ -0,0 +1,13 @@
+package cl.springmachine.file.storage.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStorageSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileStorageSpringBootApplication.class, args);
+	}
+
+}
